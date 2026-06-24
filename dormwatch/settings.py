9cb0cc839d0cf092ec731a4507d7dfdad2762313
Mykhailo B <mykhailo.bilousov.shi.2024@lpnu.ua>
2026-06-24 16:38:51 +0300

    feat: implement jwt register and login

--- dormwatch/settings.py
+++ dormwatch/settings.py
@@ -23,11 +23,11 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ug19sjvlpozd&*m%^2*$v&w7*7z$-09kf(l8dpzf_&lt2!nh)y'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-ug19sjvlpozd&*m%^2*$v&w7*7z$-09kf(l8dpzf_&lt2!nh)y')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() in ('true', '1', 'yes')
 
 ALLOWED_HOSTS = ['dormwatch-env-server.eba-jqtpnsga.us-east-1.elasticbeanstalk.com', '127.0.0.1']
 
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 AUTHENTICATION_BACKENDS = [
@@ -84,7 +85,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dormwatch.wsgi.application'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+]
+CORS_ALLOW_CREDENTIALS = True
 
 
 
@@ -150,9 +155,30 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
+        'complaints.authentication.EmailDomainJWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+}
+
+ALLOWED_EMAIL_DOMAINS = [
+    d.strip().lower()
+    for d in os.environ.get('ALLOWED_EMAIL_DOMAINS', 'lpnu.ua,example.invalid').split(',')
+    if d.strip()
+]
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_COOKIE': 'refresh_token',
+    'AUTH_COOKIE_SECURE': not DEBUG,
+    'AUTH_COOKIE_HTTP_ONLY': True,
+    'AUTH_COOKIE_SAMESITE': 'Lax',
+    'AUTH_COOKIE_PATH': '/api/auth',
 }
